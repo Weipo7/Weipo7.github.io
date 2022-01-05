0de006dd6bd41f7c0df4ffd1426f1919a6c8761e
Weipo7 <68450889+Weipo7@users.noreply.github.com>
2022-01-04 20:15:18 -0500

    hello

---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+(function () {
+  "use strict";
+
+  // ======= Sticky
+  window.onscroll = function () {
+    const ud_header = document.querySelector(".ud-header");
+    const sticky = ud_header.offsetTop;
+    const logo = document.querySelector(".navbar-brand img");
+
+    if (window.pageYOffset > sticky) {
+      ud_header.classList.add("sticky");
+    } else {
+      ud_header.classList.remove("sticky");
+    }
+
+    // === logo change
+    if (ud_header.classList.contains("sticky")) {
+      logo.src = "assets/images/ASCENTwordmarkblack.png";
+    } else {
+      logo.src = "assets/images/ASCENTwordmarkwhite.png";
+    }
+
+    // show or hide the back-top-top button
+    const backToTop = document.querySelector(".back-to-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTop.style.display = "flex";
+    } else {
+      backToTop.style.display = "none";
+    }
+  };
+
+  // ===== wow js
+  new WOW().init();
+
+  // ====== scroll top js
+  function scrollTo(element, to = 0, duration = 500) {
+    const start = element.scrollTop;
+    const change = to - start;
+    const increment = 20;
+    let currentTime = 0;
+
+    const animateScroll = () => {
+      currentTime += increment;
+
+      const val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+      element.scrollTop = val;
+
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+
+    animateScroll();
+  }
+
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  document.querySelector(".back-to-top").onclick = () => {
+    scrollTo(document.documentElement);
+  };
+})();
+
+// ====== COUNTDOWN
+// load event listeners
+loadEventListeners();
+
+function loadEventListeners() {
+	document.addEventListener('DOMContentLoaded', function() { calcTime(); });
+};
+
+var timeTo = document.getElementById('time-to').value,
+		date,
+		now = new Date(),
+		newYear = new Date("January 16, 2022 13:00:00").getTime(),
+		startTimer = '';
+
+// calculate date, hour, minute and second
+function calcTime(dates) {
+	//ui variables
+	clearInterval(startTimer);
+
+	if(typeof(dates) == 'undefined'){
+		date = new Date(newYear).getTime();
+	}else {
+		date = new Date(dates).getTime();
+	}
+
+	function updateTimer(date){
+
+		var now = new Date().getTime();
+		var distance = date - now;
+
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		// select element
+		document.querySelector('.clock-day').innerHTML = days;
+		document.querySelector('.clock-hours').innerHTML = hours;
+		document.querySelector('.clock-minutes').innerHTML = minutes;
+		document.querySelector('.clock-seconds').innerHTML = seconds;
+    document.querySelector('.clock-header').innerHTML = 'EVENT WILL START IN';
+
+		if(now >= date){
+			clearInterval(startTimer);
+			document.querySelector('.clock-day').innerHTML = 'J';
+			document.querySelector('.clock-hours').innerHTML = 'O';
+			document.querySelector('.clock-minutes').innerHTML = 'I';
+			document.querySelector('.clock-seconds').innerHTML = 'N';
+      document.querySelector('.clock-header').innerHTML = 'EVENT HAS STARTED';
+		}
+	}
+
+	startTimer = setInterval(function() { updateTimer(date); }, 1000);
+
+}
+
+//========= glightbox
+const videoTwo = GLightbox({
+  selector: ".glightbox",
+  type: "video",
+  source: "youtube", //vimeo, youtube or local
+  width: 900,
+  autoplayVideos: true,
+});
+
+
+
